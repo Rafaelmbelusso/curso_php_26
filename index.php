1120582aca056b21ed0e6e6d0efede9d1c619eac
suporte <suporte@BG-LABX-PCX.localdomain>
2025-10-14 21:45:51 -0300

    Operadores aritmeticos

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+$aviao;
+
+$aviao = "Boeing747";
+$valor1 = 10;
+$valor2 = 15;
+$soma = $valor1 + $valor2;
+$soma = $soma + 3;
+echo "A soma é: " . $soma;
+
+echo "<br>";
+
+$valor1 = 1000;
+$valor2 = 520;
+
+$subtracao = $valor1 - $valor2;
+
+echo "a subtração é: $subtracao";
+
+$valor1 = 1000;
+$valor2 = 250;
+$valor3 = 300;
+$valor4 = 5;
+$valor5 = 10;
+$valor6 = 500;
+
+echo "<br>";
+
+$calculo = $valor1 + $valor2 - $valor3 * $valor4 / $valor5;
+
+echo "A soma é : " . $calculo;
+
+echo "<br>";
+echo "<br>";
+
+echo "teste é &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; aaaaaaa";
+
+echo "<br>";
+echo "<br>";
+
+echo "Os valores são: $valor1, $valor2, $valor3, $valor5, $valor6";
+
+echo "<br>";
+echo "<br>";
+
+echo "As somas totais são: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; " . $soma + $subtracao + $calculo;
+
+
+
+
+
